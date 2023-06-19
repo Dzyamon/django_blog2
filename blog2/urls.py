54cb3ad86d3984214bf8edf4648ed5f11d319c72
Dzyamon <dmitri.olehnovich@gmail.com>
2023-06-19 16:20:18 +0300

    add new post, update post, and delete post + tests

--- blog2/urls.py
+++ blog2/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import Blog2ListView, Blog2DetailView
+from .views import Blog2ListView, Blog2DetailView, Blog2CreateView, Blog2UpdateView, Blog2DeleteView
 
 
 urlpatterns = [
+    path('post/new/', Blog2CreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', Blog2DetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/', Blog2UpdateView.as_view(), name='post_edit'),
+    path("post/<int:pk>/delete/", Blog2DeleteView.as_view(), name='post_delete'),
     path('', Blog2ListView.as_view(), name='home'),
 ]
--- blog2/views.py
+++ blog2/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class Blog2ListView(ListView):
@@ -11,3 +12,21 @@ class Blog2ListView(ListView):
 class Blog2DetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class Blog2CreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class Blog2UpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class Blog2DeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
